a8d2070784ce7947be5b4f067f037d4be3581f31
jul1ana <julianaadt97@gmail.com>
2024-02-07 19:13:47 -0300

    criacao da tabela votes

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -1,6 +1,7 @@
 import z from "zod";
 import { randomUUID } from "node:crypto";
 import { FastifyInstance } from "fastify";
+import { prisma } from "../../lib/prisma";
 
 export async function voteOnPoll(app: FastifyInstance) {
   app.post("/polls/:pollId/votes", async (req, res) => {
@@ -17,6 +18,30 @@ export async function voteOnPoll(app: FastifyInstance) {
 
     let { sessionId } = req.cookies;
 
+    //verificando se o usuario ja votou na enquete
+    if(sessionId) {
+      const userPreviousVoteOnPoll = await prisma.vote.findUnique({
+        where: {
+          sessionId_pollId: {
+            sessionId,
+            pollId
+          }
+        }
+      });
+
+      //ja votou anteriormente e a opcao que ele escolheu é diferente da opcao q esta escolhendo AGORA
+      if(userPreviousVoteOnPoll && userPreviousVoteOnPoll.pollOptionId !== pollOptionId) {
+        //apagar o voto anterior
+        await prisma.vote.delete({
+          where: {
+            id: userPreviousVoteOnPoll.id
+          }
+        });
+      } else if(userPreviousVoteOnPoll) {
+        return res.status(400).send({ message: "You already voted on this poll." });
+      };
+    };
+
     if(!sessionId) {
       sessionId = randomUUID(); 
 
@@ -28,6 +53,15 @@ export async function voteOnPoll(app: FastifyInstance) {
       });
     };
 
-    return res.status(201).send({ sessionId });
+    //criar um novo voto
+    await prisma.vote.create({
+      data: {
+        sessionId,
+        pollId,
+        pollOptionId
+      }
+    });
+
+    return res.status(201).send();
   });
 }
