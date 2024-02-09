f88cf20c9cdba99554639180dbd903d2a4f46e91
jul1ana <julianaadt97@gmail.com>
2024-02-09 10:59:53 -0300

    add comandos do redis p/ incrementacao e reducao de score

--- src/http/routes/get-poll.ts
+++ src/http/routes/get-poll.ts
@@ -1,6 +1,7 @@
 import z from "zod";
 import { prisma } from "../../lib/prisma";
 import { FastifyInstance } from "fastify";
+import { redis } from "../../lib/redis";
 
 export async function getPoll(app: FastifyInstance) {
   app.get("/polls/:pollId", async (req, res) => {
@@ -24,6 +25,42 @@ export async function getPoll(app: FastifyInstance) {
       }
     });
 
-    return res.send({ poll });
+    //caso nao encontre a enquete
+    if(!poll) {
+      return res.status(400).send({ message: "Poll not found." });
+    };
+
+    //retorna o ranking atraves de uma key(enquete)
+    //retornando da posicao 0 ate -1 -> todas as opcoes | WITHSCORES -> retorna qual a pontuacao de cada uma das opcoes
+    const result = await redis.zrange(pollId, 0, -1, "WITHSCORES");
+
+    //reduce -> metodo dentro de um Array do JS p/ converter um Array em outra estrtura
+    //percorre cada um dos itens do Array -> e executa uma funcao no item(1º parametro), 2ºparametro(qual é a estrtura inicial que quero converter este Array) -> convertendo em um objeto que vai possuir: o id da opcao e o seu voto que esta opcao possui
+    //passando qual o valor do objeto Record(é um generico - aceita 2 parametros | 1.tipo da chave do objeto(string pois sera o ID), 2.valor da chave(number pois sera o score/voto)) -> usado em objetos
+    const votes = result.reduce((obj, line, index) => { //3 parametros -> 1.objeto que esta sendo criado, 2.cada linha do Array, 3.indice
+      if(index % 2 === 0) {
+        //se o indice for PAR -> armazena na var score a opcao que vem logo depois deles -> ou seja os votos
+        const score = result[index + 1];
+
+        //assign -> mesclar 2 objetos -> mesclando a linha(o ID) c/ o valor do voto(score)
+        Object.assign(obj, { [line]: Number(score) });
+      };
+
+      return obj; //reduce smp retorna um objeto no final de cada interacao
+    }, {} as Record<string, number>);
+
+    return res.send({
+      poll: {
+        id: poll.id,
+        title: poll.title,
+        options: poll.options.map(option => {
+          return {
+            id: option.id,
+            title: option.title,
+            score: (option.id in votes) ? votes[option.id] : 0
+          };
+        })
+      }
+    });
   });
 }
--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -2,6 +2,7 @@ import z from "zod";
 import { randomUUID } from "node:crypto";
 import { FastifyInstance } from "fastify";
 import { prisma } from "../../lib/prisma";
+import { redis } from "../../lib/redis";
 
 export async function voteOnPoll(app: FastifyInstance) {
   app.post("/polls/:pollId/votes", async (req, res) => {
@@ -18,7 +19,6 @@ export async function voteOnPoll(app: FastifyInstance) {
 
     let { sessionId } = req.cookies;
 
-    //verificando se o usuario ja votou na enquete
     if(sessionId) {
       const userPreviousVoteOnPoll = await prisma.vote.findUnique({
         where: {
@@ -29,14 +29,15 @@ export async function voteOnPoll(app: FastifyInstance) {
         }
       });
 
-      //ja votou anteriormente e a opcao que ele escolheu é diferente da opcao q esta escolhendo AGORA
       if(userPreviousVoteOnPoll && userPreviousVoteOnPoll.pollOptionId !== pollOptionId) {
-        //apagar o voto anterior
         await prisma.vote.delete({
           where: {
             id: userPreviousVoteOnPoll.id
           }
         });
+
+        //descendo a pontuaçao ANTIGA apos deletar o voto acima
+        await redis.zincrby(pollId, -1, userPreviousVoteOnPoll.pollOptionId);
       } else if(userPreviousVoteOnPoll) {
         return res.status(400).send({ message: "You already voted on this poll." });
       };
@@ -53,7 +54,6 @@ export async function voteOnPoll(app: FastifyInstance) {
       });
     };
 
-    //criar um novo voto
     await prisma.vote.create({
       data: {
         sessionId,
@@ -61,6 +61,11 @@ export async function voteOnPoll(app: FastifyInstance) {
         pollOptionId
       }
     });
+
+    //incrementando os votos
+    //3 parametros obrigatorios -> 1.key = ID da enquete(pq cada enquete terá um ranking proprio) | 2.valor do voto | 3.opção da enquete
+    //incrementando em 1 voto em uma opçao de uma enquete
+    await redis.zincrby(pollId, 1, pollOptionId);
 
     return res.status(201).send();
   });
